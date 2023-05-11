1bc0731efb59ea89ce432093162e0dc7ddd58d0b
mamta925 <mamtarajput925@gmail.com>
2023-05-11 23:26:40 +0530

    new components with diff style

--- rn-starter/src/screens/ComponentsScreen.js
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -2,15 +2,20 @@ import React from "react";
 import { Text, StyleSheet , View} from "react-native";
 
 const ComponentsScreen = () => {
+  const name = 'Mamta';
   return <View>
-    <Text style={styles.textStyle}>This is from Mamta</Text>
-    <Text style={styles.textStyle}>This is from Mamta</Text>
+    <Text style={styles.textStyle}>Getting started with react Native</Text>
+    <Text style={styles.textStyle1}>This is from {name}</Text>
     </View>
 }
 
 const styles = StyleSheet.create({
   textStyle:{
-    fontSize: 30,
+    fontSize: 45,
+    color: 'black'
+  },
+  textStyle1:{
+    fontSize: 20,
     color: 'black'
   }
 });
